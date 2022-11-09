29caddfc421906c952686c9b419b3865459a5389
Lucas Melo <oliveiramelo1996@gmail.com>
2022-11-09 12:22:27 -0300

    Created project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          children: [
+            Card(child: Text("Grafico")),
+            Card(child: Text("Lista de transações")),
+          ],
+        ));
+  }
+}
